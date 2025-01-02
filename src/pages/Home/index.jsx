90469cf879cf6e94cc3153d716da8fa296891525
Juan <juan.lopez15@utp.edu.co>
2025-01-02 17:57:36 -0500

    Add dynamic banner video support and improve data handling in Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,37 +11,42 @@ import { getData } from "../../services/requests";
 const Home = () => {
     const { categories } = useContext(DataContext);
     const [videos, setVideos] = useState([]);
+    const [bannerVideo, setBannerVideo] = useState({});
+
     useEffect(() => {
-            getData().then((data) => {
-                setVideos(data);
-            });
-        }, [videos]);
+        getData().then((data) => {
+            setVideos(data);
+            setBannerVideo( {...data[0], borderColor: categories.find(category => category.name === data[0].category).color} );
+        });
+    }, [videos, bannerVideo]);
+
+
     return (
         <>
             <Header />
             <main>
                 <Banner
-                    background="https://previews.123rf.com/images/zmiter/zmiter1312/zmiter131200199/24334797-vector-comic-cartoon-merry-christmas-greeting-card.jpg"
-                    borderColor="#FF0000"
-                    category={"Christmas"}
-                    title={"Celebra la navidad"}
-                    description={"En esta navidad celebra con nosotros"}
-                    image={
-                        "https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?q=80&w=1913&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    }
+                    background="https://media.istockphoto.com/id/2139109900/photo/black-dark-blue-gray-white-abstract-background-gradient-noise-grain-granular-particle-rough.jpg?s=1024x1024&w=is&k=20&c=TaAMaS5rHTf6QLk3AZH8OmwAtandDSn8znc97n28Ceo="
+                    borderColor={bannerVideo.borderColor || "#FF0000"}
+                    category={bannerVideo.category || "Christmas"}
+                    title={bannerVideo.title || "Celebra la navidad"}
+                    description={bannerVideo.description || "En esta navidad celebra con nosotros"}
+                    image={bannerVideo.image || "https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?q=80&w=1913&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
                 ></Banner>
                 <ModalContextProvider>
                     <Modal></Modal>
                     {categories.map((category, index) => (
                         <VideoGroup
-                            videos={videos.filter(video => video.category === category.name)}
+                            videos={videos.filter(
+                                (video) => video.category === category.name
+                            )}
                             key={index}
                             category={category}
                         ></VideoGroup>
                     ))}
                 </ModalContextProvider>
             </main>
-            <Footer/>
+            <Footer />
         </>
     );
 };
